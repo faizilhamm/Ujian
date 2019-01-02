be0c9dcf9dc29ef480aa57e917d1a5f85d55ae65
AgungFatoni <afatonni1@gmail.com>
2019-01-02 16:18:12 +0700

    a

---
+++ application/views/Matematika.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body bgcolor='green' text='white'>
+	<center>
+	<h1>PILIH PAKET SOAL MATEMATIKA</h1></center><align ="right">
+		<a href="javascript:window.history.go(-1);">Kembali</a>
+	</align>
+<hr>
+
+
+
+<hr>
+<BR>
+<a href="Matematika/mtkpaket1.php"><h3><font color="white"> PAKET 1</h3></a></font>
+<BR>
+<a href="mtkpaket2.php"><h3><font color="white"> PAKET 2</h3></a></font>
+<BR>
+<a href="mtkpaket3.php"><h3><font color="white"> PAKET 3</h3></a></font>
+
+</body>
+</html>
